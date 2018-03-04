372b4a8d5520804984c28474534e35481629227c
Kuldar <hi@kuldar.com>
2018-03-04 15:05:58 +0200

    Add homepage styles

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Wait for page to load
+document.addEventListener("DOMContentLoaded", function() {
+
+  var typed = new Typed("#isTyped", {
+    strings: ["video gear", "cars", "property", "rooms"],
+    backDelay: 5000,
+    typeSpeed: 100,
+    backSpeed: 20,
+    shuffle: true,
+    loop: true
+  });
+
+});
